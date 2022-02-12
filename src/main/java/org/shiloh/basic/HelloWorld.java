a82fe9e3d716bf914b825c6635a85a610599f55e
shilohooo <Lixiaolei595@gmail.com>
2022-02-12 09:08:19 +0800

    feat: Create a sample file

---
+++ src/main/java/org/shiloh/basic/HelloWorld.java
@@ -0,0 +1,11 @@
+package org.shiloh.basic;
+
+/**
+ * @author shiloh
+ * @date 2022/2/12 9:07
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World:)");
+    }
+}
